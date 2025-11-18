97fd7669e34c91e2a3e678edca954f61aa733f5f
weardonoob <sharma.alexander@icloud.com>
2025-11-18 17:58:31 +0100

    Add files via upload

--- flappy_bird.py
+++ flappy_bird.py
@@ -9,7 +9,8 @@ floor = pygame.transform.scale(pygame.image.load("floor_flappy_bird.png"),(WIDTH
 bg = pygame.transform.scale(pygame.image.load("background_flappy_bird.png"),(WIDTH * 2,HEIGHT - HEIGHT / 3 +200))
 scroll = 0
 scroll_speed = -5
-
+flying = False
+game_over = False
 class Flappy(pygame.sprite.Sprite):
     def __init__(self):
        pygame.sprite.Sprite.__init__(self)
@@ -21,13 +22,27 @@ class Flappy(pygame.sprite.Sprite):
        self.rect = self.image.get_rect()
        self.rect.center = (100, HEIGHT / 2)
        self.counter = 0
+       self.velocity = 0
     def update(self):
-       self.counter+= 1
-       if self.counter >= 6:
-          self.counter = 0
-          self.index += 1
+       global flying, game_over
+       if not flying and not game_over and pygame.mouse.get_pressed()[0]:
+                    flying = True
+       if flying:
+            if pygame.mouse.get_pressed()[0]:
+                 self.velocity = -10
+            self.velocity += 1
+            #  print(self.velocity, self.rect.y)
+            if self.velocity > 8:
+                self.velocity = 8
+            if self.rect.y < HEIGHT * 2 / 3:
+                self.rect.y += self.velocity
+            self.counter+= 1
+            if self.counter >= 6:
+                self.counter = 0
+                self.index += 1
+        
 
-          if self.index >= 3:
+            if self.index >= 3:
                 self.index = 0
        self.image = self.images[self.index]
 group1 = pygame.sprite.Group()
@@ -44,6 +59,9 @@ while True:
         scroll = 0
     bird.update()
     group1.draw(screen)
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
 
 
 
